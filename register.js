fa3711eb5301025812014eea32c8043c7474a20b
HuuNghi2102 <huunghitc1@gmail.com>
2025-01-12 22:00:11 +0700

    update2

--- register.js
+++ register.js
@@ -3,7 +3,6 @@ const API_user = 'http://localhost:3000/user';
 const GetAPIUser = async() =>{
     try {
         const response = await fetch(API_user);
-
         if (!response.ok) {
             throw new Error('Lỗi');
         }else{
@@ -16,11 +15,7 @@ const GetAPIUser = async() =>{
     }
 }
 
-const startFunction = async() => {
-    await GetAPIUser();
-    await checkEmailUser();
-}
-startFunction();
+
 
 const checkUsername = (username)=>{
     const regex = /^[a-zA-Z]{5,}$/;
@@ -45,55 +40,75 @@ const checkEmailUser = async(emailUser) => {
         console.log(error);
     }
 }
-async function register(e){
-    // e.preventDefault();
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    if (!checkUsername(username)){
-        alert('Tên phải dài hơn 4 ký tự, không có số, dấu, hoặc khoảng cách')
-        console.log(username);
-        return;
-    }
-    if (!checkEmail(email)){
-        alert('Email không hợp lệ');
-        console.log(email);
-        return;
-    }
-    if (!checkPassword(password)){
-        alert('Mật khẩu phải dài hơn 5 ký tự, phải chứa ít nhất một ký tự in hoa, một số, và một ký tự đặc biệt');
-        console.log(password);
-        return;
-    }
-    const emailExist = await checkEmailUser(email);
-    if (emailExist) {
-        alert('Email đã tồn tại');
-        return;
-    }
-    const user = {
-        username: username,
-        email: email,
-        password: password
-    }
+const createUser = async(user) => {
     try {
-        const response = await fetch('http://localhost:3000/user', {
+        const response = await fetch(API_user, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(user),
+            body: JSON.stringify(user)
         })
-        const data = await response.json();
-        if(response.ok) {
+        if(!response.ok) {
+            throw new Error(`Lỗi`);
+        }else{
             document.getElementById('responseMessage').textContent = "Đăng ký thành công!";
             window.location.href = 'login.html'; 
-        }else{
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            const data = await response.json();
+            return data;
         }
     } catch (error) {
         document.getElementById('errorMessage').textContent = "Lỗi đăng kí!";
         console.error("Error: ", error);
     }
 }
-
+const handleCreateUser = async()=>{
+    try {
+        let registerUser = document.getElementById('registerUser');
+        registerUser.onclick = async()=>{
+            let username = document.getElementById('username').value;
+            console.log(username);
+            
+            let email = document.getElementById('email').value;
+            console.log(email);
+            
+            let password = document.getElementById('password').value;
+            console.log(password);
+            
+            if (!checkUsername(username)){
+                alert('Tên phải dài hơn 4 ký tự, không có số, dấu, hoặc khoảng cách')
+                console.log(username);
+                return;
+            }
+            if (!checkEmail(email)){
+                alert('Email không hợp lệ');
+                console.log(email);
+                return;
+            }
+            if (!checkPassword(password)){
+                alert('Mật khẩu phải dài hơn 5 ký tự, phải chứa ít nhất một ký tự in hoa, một số, và một ký tự đặc biệt');
+                console.log(password);
+                return;
+            }
+            // const emailExist = await checkEmailUser(email);
+            // if (emailExist) {
+            //     alert('Email đã tồn tại');
+            //     return;
+            // }
+            const user = {
+                username: username,
+                email: email,
+                password: password
+            }
+            await createUser(user);
+        };
+    } catch (error) {
+        console.log(error);
+    }
+}
+const startFunction = async() => {
+    await GetAPIUser();
+    await handleCreateUser();
+}
+startFunction();
 
